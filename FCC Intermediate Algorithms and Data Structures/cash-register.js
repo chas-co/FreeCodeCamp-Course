5a36a1a0274afef8dfaa74532be62e416f817a8c
Charles-Spencer Coleman <66574627+chas-co@users.noreply.github.com>
2022-03-31 09:10:25 +0100

    added 2D array listing of all currency

--- FCC Intermediate Algorithms and Data Structures/cash-register.js
+++ FCC Intermediate Algorithms and Data Structures/cash-register.js
@@ -1,1 +1,18 @@
 
+function checkCashRegister(price, cash, cid) {
+let change =[
+  ["Penny", 0.01],
+  ["Nickel",0.05],
+  ["Dime", 0.10],
+  ["Quater",0.25],
+  ["Dollar",1],
+  ["Five Dollars", 5],
+  ["Ten Dollars", 10],
+  ["Twenty Dollars", 20],
+  ["One-hundred Dollars",100],
+];
+
+
+
+}
+
