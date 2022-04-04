3729638c9d4c93bb96bd7d4527e18e7392ee9ca6
Charles-Spencer Coleman <66574627+chas-co@users.noreply.github.com>
2022-04-04 19:38:14 +0100

    Added preprocessing of function arguments

--- FCC Intermediate Algorithms and Data Structures/cash-register.js
+++ FCC Intermediate Algorithms and Data Structures/cash-register.js
@@ -10,7 +10,7 @@ Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for th
 
 Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.*/
 
-//Obj
+//currency object containing all the denominations
 const currency={
   "ONE HUNDRED": 100*100,
   "TWENTY":20*100,
@@ -23,7 +23,30 @@ const currency={
   "PENNY": 0.01*100
 };
 function checkCashRegister(price, cash, cid) {
-
+  
+ // Pre-processing of the arguments passed to the function before applying cash register logic
+ // We multiply all the values by hundred to avoid errors that occur in js with handling floating point numbers
+ //--------------------------------------------------------------------------------------------------------------------
+  // convert the cid 2D array to an object
+  let cidObj = {};
+  cid.map(item => cidObj[item[0]]=item[1]*100);
+  
+  // Collect the various denominations into an array
+  let denominations = Object.keys(currency);
+  
+  // calculate the amount of money in the cash register
+  let cidValue = cid.reduce((a,b)=> a+b[1],0)*100;
+ 
+  //calculate the amount of money due as change
+  let changeDue = (cash-price)*100;
+ 
+  // declare an array to store the denominations to be issued as change
+   let change =[];
+  
+  
+  // Cash register logic
+  //----------------------------------------------------------------------------------------------------------------------------
+  
 
 
 
