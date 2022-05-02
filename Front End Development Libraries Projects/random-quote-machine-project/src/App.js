2e0867d173e6bc3aca4c578c67af7d1e7dd29549
Charles-Spencer Coleman <spencole@gmail.com>
2022-05-02 22:08:39 +0100

    added quote and author states to be rendered

--- Front End Development Libraries Projects/random-quote-machine-project/src/App.js
+++ Front End Development Libraries Projects/random-quote-machine-project/src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+import React,{useState, useEffect} from 'react';
+import './App.scss';
+
+let quotesLink= 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 
 function App() {
+  const [quote, setQuote] = useState("What is dead may never die");
+  const [author, setAuthor] = useState("Iron Born");
+  const [quotesArray, setQuotesArray] = useState([]);
+
+  
+  
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello World
-        </p>
-        
+        <div id= "quote-box">
+        <div id="text">"{quote}"</div>
+        <div id="author">- {author}</div>
+       
+        </div>
       </header>
     </div>
   );
